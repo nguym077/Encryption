2f383f76f363aaacd68834de59f65f671b290b48
Marian Nguyen <itsmariann@gmail.com>
2018-03-11 23:01:22 -0700

    renamed files

---
+++ decrypt.py
@@ -0,0 +1,14 @@
+# Marian Nguyen
+# Steven Chung
+# Lab 2 -- File Encryption
+# March 15, 2018
+
+# You'll have to write the inverse of the methods in Encrypt.py **
+
+
+def mydecrypt(message, key):
+    print('test')
+
+
+def myfiledecrypt(filepath):
+    print("test 2")
