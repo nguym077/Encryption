90217862013d6e0cc91d81f0ae141871f030dcaa
Steven Chung <stevenchung@dhcp-39-37-194.dhcpint.csulb.edu>
2018-03-14 21:58:04 -0700

    Hello

--- decrypt.py
+++ decrypt.py
@@ -4,10 +4,9 @@
 # March 15, 2018
 
 # You'll have to write the inverse of the methods in Encrypt.py **
+#print("Hello World")
 
-print("Hello World")
 
-#I'm soo lost
 
 def mydecrypt(message, key):
     print("test")
