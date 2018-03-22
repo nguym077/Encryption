16686d1b41392be5c8f88085a99e2123494f4c96
Marian Nguyen <itsmariann@gmail.com>
2018-03-21 21:53:18 -0700

    i think I have myrsadecrypt working? not sure..

--- main.py
+++ main.py
@@ -3,40 +3,9 @@
 # Lab 2 -- File Encryption
 # March 15, 2018
 
-from constants import KEY_SIZE, FERMAT_PRIME
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+from rsa import generateRSAKeys, MyRSADecrypt, MyRSAEncrypt
+from encrypt import myfileencrypt
 
-# generates new RSA private key
-# value used for public_exponent is default
-private_key = rsa.generate_private_key(
-    public_exponent=FERMAT_PRIME,
-    key_size=KEY_SIZE,
-    backend=default_backend()
-)
+# generateRSAKeys()
 
-# generates public key from private key
-public_key = private_key.public_key()
-
-# serializes (private) key
-private_pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-)
-
-# serializes (public) key
-public_pem = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
-
-# writes keys to file
-fh = open("files/rsa_private_key.pem", "wb")
-fh.write(private_pem)
-fh.close()
-
-fh = open("files/rsa_public_key.pem", "wb")
-fh.write(public_pem)
-fh.close()
+myfileencrypt("files/TestImage.jpg")
--- rsa.py
+++ rsa.py
@@ -3,7 +3,77 @@
 # Lab 2 -- File Encryption
 # March 15, 2018
 
-# You'll have to write the inverse of the above methods. **
+import sys
+import os
+import base64
+
+from encrypt import myfileencrypt
+from decrypt import myfiledecrypt
+from constants import FERMAT_PRIME, KEY_SIZE
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import algorithms, modes, Cipher
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+
+def generateRSAKeys():
+    # generates new RSA private key
+    # value used for public_exponent is default
+    private_key = rsa.generate_private_key(
+        public_exponent=FERMAT_PRIME,
+        key_size=KEY_SIZE,
+        backend=default_backend()
+    )
+
+    # generates public key from private key
+    public_key = private_key.public_key()
+
+    # serializes (private) key
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    # serializes (public) key
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    # writes keys to file
+    fh = open("files/rsa_private_key.pem", "wb")
+    fh.write(private_pem)
+    fh.close()
+
+    fh = open("files/rsa_public_key.pem", "wb")
+    fh.write(public_pem)
+    fh.close()
+
+
+def MyRSAEncrypt(filepath, RSA_Publickey_filepath):
+    c, iv, key, ext = myfileencrypt(filepath)
+
+    fh = open(RSA_Publickey_filepath, "rb")
+    public_key = serialization.load_pem_public_key(
+        fh.read(),
+        backend=default_backend()
+    )
+    fh.close()
+
+    RSACipher = public_key.encrypt(
+        key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return RSACipher, c, iv, ext
 
 # (RSACipher, C, IV, ext)= MyRSAEncrypt(filepath, RSA_Publickey_filepath):
 # In this method, you first call MyfileEncrypt (filepath) which will
@@ -16,42 +86,23 @@
 # does the exactly inverse of the above and generate the decrypted
 # file using your previous decryption methods.
 
-# openssl(rsa keygen)
 
-import sys
-import os
-import base64
-
-from encrypt import myfileencrypt
-from constants import KEY_BYTES, BLOCK_SIZE, IV_BYTES
-
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.ciphers import algorithms, modes, Cipher
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives.padding import PKCS7
-from cryptography.hazmat.primitives import asymmetric
-from cryptography.hazmat.primitives import serialization
-
-
-def MyRSAEncrypt(filepath, RSA_Publickey_filepath):
-    c, iv, key, ext = myfileencrypt("./" + filepath)
-
-    fh = open(RSA_Publickey_filepath, "rb")
-    publicKey = serialization.load_pem_public_key(
+def MyRSADecrypt(RSACipher, c, iv, ext, RSA_privatekey_filepath):
+    fh = open(RSA_privatekey_filepath, "rb")
+    private_key = serialization.load_pem_private_key(
         fh.read(),
+        password=None,
         backend=default_backend()
     )
+    fh.close()
 
-    RSACipher = publicKey.encrypt(
-        key,
-        asymmetric.padding.OAEP(
-            x=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+    RSAplaintext = private_key.decrypt(
+        RSACipher,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
-
         )
     )
 
-    return RSACipher, c, iv, ext
-
+    return RSAplaintext
